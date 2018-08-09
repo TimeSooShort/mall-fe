9610ee6061ff97e6025583ebc50b718c44448eac
Miao <1160292058@qq.com>
2018-08-10 01:55:34 +0800

    index page

--- src/page/indexs/index.js
+++ src/page/indexs/index.js
@@ -2,11 +2,27 @@
 * @Author: Miao
 * @Date:   2018-07-29 00:57:17
 * @Last Modified by:   Miao
-* @Last Modified time: 2018-08-04 18:16:25
+* @Last Modified time: 2018-08-10 01:45:01
 */
 'use strict';
-var _mm = require('util/mm.js');
+require('./index.css');
 require('page/common/header/index.js');
 require('page/common/nav/index.js');
-var navSide = require('page/common/nav-side/index.js');
-navSide.init({name : 'about'});
+require('util/slider/index.js');
+var _mm = require('util/mm.js');
+var templateBanner = require('./banner.string');
+
+$(function() {
+    // 渲染banner的html
+    var bannerHtml = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider = $('.banner').unslider({
+        dots: true
+    });
+    // 前一个与后一个箭头事件的绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
+});
---
+++ src/util/slider/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Miao
+* @Date:   2018-08-10 00:20:42
+* @Last Modified by:   Miao
+* @Last Modified time: 2018-08-10 00:21:52
+*/
+'use strict';
+
+require('./index.css');
+require('./unslider.js');
